e630b217f34329946c0ab2d7c06ce00394db566d
Zuleiha Dantijani <tzuleiha@gmail.com>
2021-12-26 18:57:57 +0100

    home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,9 +4,7 @@ const Home = () => {
     return (
         <div className = "home">
             <div className = "title">
-                <h1>
-                    <p>Zuleiha Dantijani</p>
-                </h1>
+                <h1>Zuleiha Dantijani</h1>
                 <h4>Full stack Software engineer</h4>
                 <p>I am an innovative Software engineer with 2 years of experience building 
                     interfaces that are usable and pleasant for the most number of people possible.
@@ -16,10 +14,10 @@ const Home = () => {
                 </Link>
             </div> 
             <div className = "person">
-                    <img
-                    src={`${process.env.PUBLIC_URL}/profile_p.jpeg`}
-                    alt="mypicture"
-                    />
+                <img
+                src={`${process.env.PUBLIC_URL}/profile_p.jpeg`}
+                alt="mypicture"
+                />
             </div>    
         </div>
     );
